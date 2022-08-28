49705082ec4dc0a081dc6b6cc58ad655af3be449
RohitAgarwal9639 <109723666+RohitAgarwal9639@users.noreply.github.com>
2022-08-28 10:53:44 -0500

    Update 231-power-of-two.java

--- 231-power-of-two/231-power-of-two.java
+++ 231-power-of-two/231-power-of-two.java
@@ -1,13 +1,16 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
+        //initialize variable c for counting number of 1's in binary representation of n
         int c=0;
         while(n!=0)
         {
             int a=n&1;
             if(a==1)
                 c++;
+            // if c>1 means it can not be power of 2 so break
             if(c>1)
                 break;
+            //right shift number by 1 bit
             n>>=1;
         }
         if(c==1)
